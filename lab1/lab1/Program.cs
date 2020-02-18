08c80c24cfc1038a273960cc54278f71386df1cc
THE DEAD <fpv555@gmail.com>
2020-02-18 09:39:45 +0500

    first commit

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Program
+    {
+        static string str;
+        static bool cout(string inc)
+        {
+            Console.WriteLine(inc);
+            return (true);
+        }
+        static string cin()
+        {
+            str = Console.ReadLine();
+            return (str);
+        }
+        static void Main(string[] args)
+        {
+            if (cout(cin()) == true)
+            {
+                cout("if succeeded!");
+            }
+        } 
+    }
+}
